f1b98e31223f97f4f823f48c19a47df0c7ba86d4
Binish Manandhar <binish784@hotmail.com>
2018-03-29 18:32:38 +0545

    Rule 1 added | second commit

--- playFair-cipher.py
+++ playFair-cipher.py
@@ -7,11 +7,11 @@ Created on Thu Mar 29 15:51:51 2018
 
 class playFair:
     
-    w,h=5,5
     final_list=[]
         
     def __init__(self):
-        self.key=[[0 for x in range(self.w)] for y in range(self.h)]
+        w,h=5,5
+        self.key=[[0 for x in range(w)] for y in range(h)]
         
     def defineKeyword(self,key_letters):
         key_letters=key_letters.replace("j","i")
@@ -33,9 +33,31 @@ class playFair:
             for j in range(5):
                 self.key[i][j]=self.final_list[a]
                 a=a+1
-                row=row+self.key[i][j]
+                row=row+self.key[i][j]+" "                
             print("\t"+row)
             
+    def encrypt(self,plain_text):
+        print("\nstep 1 : "+ str(self.rule1(plain_text)))
+            
+    def rule1(self,plain_text):
+        new_plain_list=[]
+        plain_list=list(plain_text)
+        while (len(plain_list)>1):
+            new_plain_list.append(plain_list[0])
+            if(plain_list[0]!=plain_list[1]):
+                new_plain_list.append(plain_list[1])
+                del plain_list[1]
+            elif(plain_list[0]==plain_list[1]):
+                new_plain_list.append('x')
+            del plain_list[0]
+        if(len(plain_list)==1):
+            new_plain_list.append(plain_list[0])
+            new_plain_list.append('x')
+        return(new_plain_list)
+     
+        
+print("\n\tPlay Fair Cipher\n")    
 x=playFair()
-x.defineKeyword("monarchjiji")
+x.defineKeyword("monarchy")
 x.makeKey()
+x.encrypt("balloon")
